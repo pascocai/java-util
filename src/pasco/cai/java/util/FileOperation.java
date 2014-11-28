37cce32cee90e19c6caa23dd6869b34b6514021a
Pasco Cai <pascochoi@hotmail.com>
2014-11-28 14:59:33 +0800

    支持utf-8

--- src/pasco/cai/java/util/FileOperation.java
+++ src/pasco/cai/java/util/FileOperation.java
@@ -4,101 +4,31 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 
 public class FileOperation {
-	
-	private String newLineChar[] = {"<br>", "\r\n"}; 
-
-	public void write(int logType, String path, String content) {
-	      String s = new String();
-	      String s1 = new String();
-	      if(logType==1)
-	    	  System.out.println(content);
-	      else{
-	    	  try {
-		    	  File f = new File(path);
-		    	  if (f.exists()) {
-		    		  System.out.println("文件存在");
-		    	  } else {
-		    		  System.out.println("文件不存在，正在創建...");
-		    		  if (f.createNewFile()) {
-		    			  System.out.println("文件創建成功！");
-		    		  } else {
-		    			  System.out.println("文件創建失敗！");
-		    		  }
-		    	  }
-		    	  BufferedReader input = new BufferedReader(new FileReader(f));
-		    	  while ((s = input.readLine()) != null) {
-		    		  s1 = s1 + s + newLineChar[1];
-		    	  }
-		    	  System.out.println("文件內容：" + s1);
-		    	  input.close();
-		    	  s1 += content;
-
-		    	  BufferedWriter output = new BufferedWriter(new FileWriter(f));
-		    	  output.write(s1);
-		    	  output.close();
-		      } catch (Exception e) {
-		    	  e.printStackTrace();
-		      }
-	      }
-	}
-	
-	public void write(int logType, String path, String content, String color) {
-	      String s = new String();
-	      String s1 = new String();
-	      if(logType==1){
-	    	  System.out.println(content);
-	      } else{
-	    	  try {
-		    	  File f = new File(path);
-		    	  if (f.exists()) {
-		    		  System.out.println("文件存在");
-		    	  } else {
-		    		  System.out.println("文件不存在，正在創建...");
-		    		  if (f.createNewFile()) {
-		    			  System.out.println("文件創建成功！");
-		    		  } else {
-		    			  System.out.println("文件創建失敗！");
-		    		  }
-		    	  }
-		    	  BufferedReader input = new BufferedReader(new FileReader(f));
-		    	  while ((s = input.readLine()) != null) {
-		    		  s1 = s1 + s + newLineChar[logType];
-		    	  }
-		    	  System.out.println("文件內容：" + s1);
-		    	  input.close();
-		    	  content = "<span style=\"color:" + color + "\">" + content + "</span>";
-		    	  s1 += content;
-
-		    	  BufferedWriter output = new BufferedWriter(new FileWriter(f));
-		    	  output.write(s1);
-		    	  output.close();
-		      } catch (Exception e) {
-		    	  e.printStackTrace();
-		      }
-	      }
-	}
 	
 	public void write(String path, String content) {
 	      String oldStr = new String();
 	      String newStr = new String();
 	      try {
-	    	  File f = new File(path);
-	    	  if (f.exists()) {
+	    	  File file = new File(path);
+	    	  if (file.exists()) {
 	    		  //System.out.println("文件存在");
 	    	  } else {
 	    		  //System.out.println("文件不存在，正在創建...");
-	    		  if (f.createNewFile()) {
+	    		  if (file.createNewFile()) {
 	    			  //System.out.println("文件創建成功！");
 	    		  } else {
 	    			  //System.out.println("文件創建失敗！");
 	    		  }
 	    	  }
-	    	  BufferedReader input = new BufferedReader(new FileReader(f));
+	    	  BufferedReader input = new BufferedReader(new InputStreamReader(new FileInputStream(file), "utf-8"));
 	    	  while ((oldStr = input.readLine()) != null) {
 	    		  newStr += oldStr;
 	    	  }
@@ -106,7 +36,7 @@ public class FileOperation {
 	    	  input.close();
 	    	  newStr += content;
 
-	    	  BufferedWriter output = new BufferedWriter(new FileWriter(f));
+	    	  PrintWriter output = new PrintWriter(new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file),"utf-8")));
 	    	  output.write(newStr);
 	    	  output.close();
 	      } catch (Exception e) {
@@ -119,9 +49,9 @@ public class FileOperation {
 		StringBuffer buffer = new StringBuffer();
 		File file = new File(filename);
 		if (file.exists()) {
-			System.out.println("文件存在！");
+			//System.out.println("文件存在！");
 			try {
-				BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+				BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "utf-8"));
 				while ((str = reader.readLine()) != null) {
 					buffer.append(str);
 				}
@@ -143,11 +73,10 @@ public class FileOperation {
 		if (file.exists()) {
 			//System.out.println("文件存在！");
 			try {
-				BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+				BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "utf-8"));
 				while ((str = reader.readLine()) != null) {
 					buffer.append(str);
 				}
-				//System.out.println(buffer);
 				returnStr = buffer.toString();
 				reader.close();
 			} catch (Exception e) {
